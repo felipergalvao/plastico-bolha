f081f3c290c64a7118c665a555fa0a29e9f63d3b
felipergalvao <felipe.rgalvao@hotmail.com>
2026-01-29 15:50:37 -0300

    fix: correct gradle kts syntax and update app id

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,17 +19,17 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        defaultConfig {
+        // Note o sinal de igual (=)
         applicationId = "com.galvaoapps.bubbletycoon"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdkVersion 21
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        
+        // Note os parênteses () no lugar de espaço
+        minSdkVersion(21)
+        targetSdkVersion(flutter.targetSdkVersion)
+        versionCode = flutterVersionCode.toInteger()
+        versionName = flutterVersionName
     }
+
 
     buildTypes {
         release {
